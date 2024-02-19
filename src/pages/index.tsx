b290a9740c43b738058571766e934c5ee4fd26df
Ekaterina Levenkova <levenkovaes@gmail.com>
2024-02-19 23:47:48 +0100

    Add No Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from "../shared/ui/layout";
 import Home from "./home/ui";
 import Menu from "./menu/ui";
 import PackingChecklist from "./packing-checklist/ui";
+import NoPage from "./no-page";
 
 const Pages = () => (
   <Routes>
@@ -11,6 +12,8 @@ const Pages = () => (
     <Route path="/" element={<Layout />}>
       <Route path="menu" element={<Menu />} />
       <Route path="packing-checklist" element={<PackingChecklist />} />
+
+      <Route path="*" element={<NoPage />} />
     </Route>
   </Routes>
 );
---
+++ src/pages/no-page/styled.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const NoPageContainer = styled.div`
+  align-self: center;
+  text-align: center;
+`;
